5fbf6a3a8f829ede0f4303fa3d3b8e3bb885eaa8
Masood Abdolian <m.abdolian@gmail.com>
2023-02-14 22:24:36 +0330

    Added fieldset

---
+++ src/lib/fieldset/Fieldset.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { El } from '../el'
+	import type { FieldsetProps } from './Fieldset.types'
+
+	type $$Props = FieldsetProps
+
+	export let componentName: $$Props['componentName'] = 'fieldset'
+
+	let cssProps: FieldsetProps = {}
+	let props: FieldsetProps = {}
+	$: {
+		cssProps = {}
+		props = {
+			componentName,
+		}
+	}
+</script>
+
+<El {...$$restProps} {cssProps} {...props}>
+	<slot />
+</El>
---
+++ src/routes/docs/fieldset/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { Page, Doc, Preview } from '$components'
+	import FieldsetDefault from './FieldsetDefault.svelte'
+</script>
+
+<Page
+	title="Fieldset"
+	description="Group parts of your form to make it well-structured and clearer for users, using the Fieldset element.">
+	<Doc title="Defaults" description="A simple usage">
+		<Preview src="./FieldsetDefault.svelte">
+			<FieldsetDefault />
+		</Preview>
+	</Doc>
+</Page>
