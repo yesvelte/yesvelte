ef66026e1a0e8f4bab893a32653a506dd34ed2b0
Masood Abdolian <m.abdolian@gmail.com>
2023-02-19 20:44:22 +0330

    added link example for the alert

--- src/routes/docs/alert/AlertLink.svelte
+++ src/routes/docs/alert/AlertLink.svelte
@@ -2,4 +2,15 @@
 	import { Alert } from 'yesvelte'
 </script>
 
-TODO
+<Alert color="success">
+	This is a success alert — <a href="#" class="alert-link">check it out</a>!
+</Alert>
+<Alert color="info">
+	This is a info alert — <a href="#" class="alert-link">check it out</a>!
+</Alert>
+<Alert color="warning">
+	This is a warning alert — <a href="#" class="alert-link">check it out</a>!
+</Alert>
+<Alert color="danger">
+	This is a danger alert — <a href="#" class="alert-link">check it out</a>!
+</Alert>
