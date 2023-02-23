178f0b070f1745239ec73356b6ea2d5b72e42b19
Masood Abdolian <m.abdolian@gmail.com>
2023-02-23 23:47:03 +0330

    init the button

--- src/lib/button/Button.types.ts
+++ src/lib/button/Button.types.ts
@@ -1,25 +1,17 @@
 import type { Colors, ElProps } from '../el'
 
-// Button Types
-declare type ButtonTypes = 'submit' | 'button' | 'reset' | undefined
-declare type ButtonSizes = 'sm' | 'lg' | undefined
-declare type ButtonValues = string | undefined
-
 export interface ButtonProps extends Partial<ElProps> {
-	color?: Colors
-	outline?: boolean
-	link?: boolean
-	type?: ButtonTypes
-	value?: ButtonValues
-	size?: ButtonSizes
-	loading?: boolean
 	active?: boolean
+	color?: Colors
 	disabled?: boolean
-	toggle?: boolean
-	pill?: boolean
-	tile?: boolean
 	ghost?: boolean
 	href?: string
-	role?: string
-	'aria-label'?: string
+	icon?: boolean
+	link?: boolean
+	loading?: boolean
+	outline?: boolean
+	shape?: 'tile' | 'pill' | 'round' | 'fab'
+	size?: 'sm' | 'md' | 'lg'
+	target?: '_blank' | '_self' | '_parent' | '_top'
+	type?: 'button' | 'reset' | 'submit'
 }
---
+++ src/routes/docs/button/ButtonTarget.svelte
@@ -0,0 +1,5 @@
+<script>
+	import { Button } from 'yesvelte'
+</script>
+
+<Button>Button</Button>
