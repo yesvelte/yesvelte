da3017a5444afa7822c2c8c1cb1db7e88d70b8e7
Amir Pournasserian <ap@momentaj.com>
2023-02-25 14:14:50 -0500

    #47 Avatar component refactor

--- src/routes/docs/avatar/AvatarList.svelte
+++ src/routes/docs/avatar/AvatarList.svelte
@@ -3,11 +3,15 @@
 </script>
 
 <AvatarList>
-	<Avatar><img src="https://preview.tabler.io/static/avatars/000m.jpg" alt="" /></Avatar>
+	<Avatar>
+		<img src="https://preview.tabler.io/static/avatars/000m.jpg" alt="" />
+	</Avatar>
 	<Avatar>
 		<Icon name="user" />
 	</Avatar>
-	<Avatar><img src="https://preview.tabler.io/static/avatars/002f.jpg" alt="" /></Avatar>
+	<Avatar>
+		<img src="https://preview.tabler.io/static/avatars/002f.jpg" alt="" />
+	</Avatar>
 	<Avatar>
 		<Icon name="plus" />
 	</Avatar>
