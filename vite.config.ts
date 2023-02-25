69b72592165666e33f180cf087d36835ce17595c
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-02-25 12:52:10 +0300

    use a vite plugin to watch scss changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+import path from 'path'
+import watchAndRun from 'vite-plugin-watch-and-run'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 
+
 export default defineConfig({
 	server: { port: 3000 },
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		watchAndRun([
+			{
+			  name: 'generate styles',
+			  watchKind: ['add', 'change', 'unlink'],
+			  watch: path.resolve('src/scss/**/*.scss'),
+			  run: 'npm run style',
+			  delay: 300,
+			}
+		])
+	],
 })
