2f9c1be7f4e37644b1c7aa5ba5a2988f71e0d250
Amir Pournasserian <amir.pournasserian@live.com>
2023-02-26 16:15:50 -0500

    #90 documentation updated for using ButtonGroup

--- src/routes/docs/alert/AlertButtons.svelte
+++ src/routes/docs/alert/AlertButtons.svelte
@@ -6,35 +6,27 @@
 	<p>
 		Lorem ipsum Minim ad pariatur eiusmod ea ut nulla aliqua est quis id dolore minim voluptate.
 	</p>
-	<!-- <ButtonGroup> -->
 	<Button color="success">Okay</Button>
 	<Button outline>Cancel</Button>
-	<!-- </ButtonGroup> -->
 </Alert>
 <Alert dismissible title="Some Title" color="info">
 	<p>
 		Lorem ipsum Minim ad pariatur eiusmod ea ut nulla aliqua est quis id dolore minim voluptate.
 	</p>
-	<!-- <ButtonGroup> -->
 	<Button color="info">Okay</Button>
 	<Button outline>Cancel</Button>
-	<!-- </ButtonGroup> -->
 </Alert>
 <Alert dismissible title="Some Title" color="warning">
 	<p>
 		Lorem ipsum Minim ad pariatur eiusmod ea ut nulla aliqua est quis id dolore minim voluptate.
 	</p>
-	<!-- <ButtonGroup> -->
 	<Button color="warning">Okay</Button>
 	<Button outline>Cancel</Button>
-	<!-- </ButtonGroup> -->
 </Alert>
 <Alert dismissible title="Some Title" color="danger">
 	<p>
 		Lorem ipsum Minim ad pariatur eiusmod ea ut nulla aliqua est quis id dolore minim voluptate.
 	</p>
-	<!-- <ButtonGroup> -->
 	<Button color="danger">Okay</Button>
 	<Button outline>Cancel</Button>
-	<!-- </ButtonGroup> -->
 </Alert>
