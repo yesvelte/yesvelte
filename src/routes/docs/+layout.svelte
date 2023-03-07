d02e3641b32754aa2ec5685021fecffb8771bd3b
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-03-07 20:06:24 +0300

    fix conflict

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <svelte:head>
-	<link rel="stylesheet" href="/css/tabler.css" />
+	<!-- <link rel="stylesheet" href="/css/tabler.css" /> -->
 	<meta name="robots" content="index, follow" />
 	<meta name="author" content="Amir Pournasserian" />
 </svelte:head>
