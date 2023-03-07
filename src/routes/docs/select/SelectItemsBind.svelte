40b63b39cb79c2814b164e3348291a3cb894cd53
Mohammad Taherian <mhmdtaherian@gmail.com>
2023-03-07 13:58:44 -0500

    added array of object example

---
+++ src/routes/docs/select/SelectItemsBind.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { Select } from 'yesvelte'
+
+	let items = [
+		{
+			name: 'Apple',
+			id: '1',
+			type: 'Fruit',
+		},
+		{
+			name: 'Orange',
+			id: '2',
+			type: 'Fruit',
+		},
+		{
+			name: 'Peach',
+			id: '3',
+			type: 'Fruit',
+		},
+		{
+			name: 'Banana',
+			id: '4',
+			type: 'Fruit',
+		},
+		{
+			name: 'Apricot',
+			id: '5',
+			type: 'Fruit',
+		},
+	]
+
+	let defaultId = '1'
+	let value: any = items.find((x) => x.id === defaultId)
+
+	$: selectedId = value?.id
+</script>
+
+<Select bind:value {items} let:item let:index>{item.id} - {item.name}</Select>
+
+<i>Selected Id: {selectedId} - value: {JSON.stringify(value)}</i>
