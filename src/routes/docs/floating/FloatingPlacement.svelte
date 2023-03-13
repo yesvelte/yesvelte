9ebf2bc1d5ae0ba798f815139c30425fd752adee
Masood Abdolian <m.abdolian@gmail.com>
2023-03-13 19:44:32 +0330

    init the floating component

---
+++ src/routes/docs/floating/FloatingPlacement.svelte
@@ -0,0 +1,15 @@
+<script>
+	import CardBody from '$lib/card/CardBody.svelte'
+	import { Card, Dot, Floating } from 'yesvelte'
+</script>
+
+<Card>
+	<CardBody>
+		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+		the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+		of type and scrambled it to make a type specimen book.
+	</CardBody>
+	<Floating placement="start-bottom">
+		<Dot color="red" />
+	</Floating>
+</Card>
