7bd612adc7ed5efe21e69ffe71f2dd4b7e8ac360
Mohammad Taherian <mhmdtaherian@gmail.com>
2023-04-21 18:00:15 -0400

    implemented widgets page

--- src/routes/demo/widgets/+page.svelte
+++ src/routes/demo/widgets/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import Avatar from '$lib/avatar/Avatar.svelte'
 	import CardBody from '$lib/card/CardBody.svelte'
+	import CardTitle from '$lib/card/CardTitle.svelte'
+	import Dot from '$lib/dot/Dot.svelte'
 	import {
 		El,
 		Card,
@@ -10,11 +12,14 @@
 		Dropdown,
 		DropdownMenu,
 		DropdownItem,
+		Button,
+		CardMedia,
+		Label,
 	} from 'yesvelte'
 </script>
 
 <El row>
-	<El colMd="6" border>
+	<El colMd="6" mb="3">
 		<Card>
 			<CardBody alignItems="center">
 				<El row>
@@ -31,7 +36,7 @@
 							</El>
 						</El>
 					</El>
-					<El col="auto">
+					<El col="auto" alignSelf="center">
 						<Dropdown placement="bottom-end" arrow={false}>
 							<Icon name="dots-vertical" slot="target" />
 							<DropdownMenu>
@@ -46,7 +51,7 @@
 			</CardBody>
 		</Card>
 	</El>
-	<El colMd="6" border>
+	<El colMd="6" mb="3">
 		<Card>
 			<CardBody alignItems="center">
 				<El row>
@@ -63,7 +68,7 @@
 							</El>
 						</El>
 					</El>
-					<El col="auto">
+					<El col="auto" alignSelf="center">
 						<Dropdown placement="bottom-end" arrow={false}>
 							<Icon name="dots-vertical" slot="target" />
 							<DropdownMenu>
@@ -78,7 +83,7 @@
 			</CardBody>
 		</Card>
 	</El>
-	<El colSm="6" colLg="3" border>
+	<El colSm="6" colLg="3" mb="3">
 		<Card>
 			<CardBody row alignItems="center">
 				<El col="auto">
@@ -93,7 +98,7 @@
 			</CardBody>
 		</Card>
 	</El>
-	<El colSm="6" colLg="3" border>
+	<El colSm="6" colLg="3" mb="3">
 		<Card>
 			<CardBody row alignItems="center">
 				<El col="auto">
@@ -106,7 +111,7 @@
 			</CardBody>
 		</Card>
 	</El>
-	<El colSm="6" colLg="3" border>
+	<El colSm="6" colLg="3" mb="3">
 		<Card>
 			<CardBody row alignItems="center">
 				<El col="auto">
@@ -119,7 +124,7 @@
 			</CardBody>
 		</Card>
 	</El>
-	<El colSm="6" colLg="3" border>
+	<El colSm="6" colLg="3" mb="3">
 		<Card>
 			<CardBody row alignItems="center">
 				<El col="auto">
@@ -132,12 +137,217 @@
 			</CardBody>
 		</Card>
 	</El>
-</El>
-<El row>
-	<El col="6" border>aa</El>
-	<El col="6" border>aa</El>
-	<El col="3" border>aa</El>
-	<El col="3" border>aa</El>
-	<El col="3" border>aa</El>
-	<El col="3" border>aa</El>
+	<El colSm="6" colLg="3" mb="3">
+		<Card>
+			<CardBody textAlign="center">
+				<Avatar size="xl" mb="3">
+					<img src="/images/avatars/010m.jpg" alt="" />
+				</Avatar>
+				<CardTitle mb="1">Thatcher Keel</CardTitle>
+				<El textMuted>VP Sales</El>
+			</CardBody>
+			<Button col="12" p="3">View full profile</Button>
+		</Card>
+	</El>
+	<El colSm="6" colLg="3" mb="3">
+		<Card>
+			<CardBody textAlign="center">
+				<Avatar size="xl" mb="3">
+					<img src="/images/avatars/005f.jpg" alt="" />
+				</Avatar>
+				<CardTitle mb="1">Dyann Escala</CardTitle>
+				<El textMuted>Mechanical Systems Engineer</El>
+			</CardBody>
+			<Button col="12" p="3">View full profile</Button>
+		</Card>
+	</El>
+	<El colSm="6" colLg="3" mb="3">
+		<Card>
+			<CardBody textAlign="center">
+				<Avatar size="xl" mb="3">
+					<img src="/images/avatars/006f.jpg" alt="" />
+				</Avatar>
+				<CardTitle mb="1">Avivah Mugleston</CardTitle>
+				<El textMuted>Actuary</El>
+			</CardBody>
+			<Button col="12" p="3">View full profile</Button>
+		</Card>
+	</El>
+	<El colSm="6" colLg="3" mb="3">
+		<Card>
+			<CardBody textAlign="center">
+				<Avatar size="xl" mb="3">AA</Avatar>
+				<CardTitle mb="1">Arlie Armstead</CardTitle>
+				<El textMuted>VP Quality Control</El>
+			</CardBody>
+			<Button col="12" p="3">View full profile</Button>
+		</Card>
+	</El>
+
+	<El colSm="6" colLg="3" mb="3">
+		<Card>
+			<CardMedia elementPosition="top" textAlign="center">
+				<img src="/images/avatars/blond-using-her-laptop-at-her-bedroom.jpg" alt="" />
+			</CardMedia>
+			<CardBody textAlign="center">
+				<CardTitle mb="1">Marsha Labat</CardTitle>
+				<El textMuted>Research Associate</El>
+			</CardBody>
+		</Card>
+	</El>
+	<El colSm="6" colLg="3" mb="3">
+		<Card>
+			<CardMedia elementPosition="top" textAlign="center">
+				<img src="/images/avatars/tropical-palm-leaves-floral-pattern-background.jpg" alt="" />
+			</CardMedia>
+			<CardBody textAlign="center">
+				<CardTitle mb="1">Elston Muffett</CardTitle>
+				<El textMuted>Account Coordinator</El>
+			</CardBody>
+		</Card>
+	</El>
+	<El colSm="6" colLg="3" mb="3">
+		<Card>
+			<CardMedia elementPosition="top" textAlign="center">
+				<img src="/images/avatars/woman-read-book-and-drink-coffee.jpg" alt="" />
+			</CardMedia>
+			<CardBody textAlign="center">
+				<CardTitle mb="1">Leigha Gorce</CardTitle>
+				<El textMuted>Geologist II</El>
+			</CardBody>
+		</Card>
+	</El>
+	<El colSm="6" colLg="3" mb="3">
+		<Card>
+			<CardMedia elementPosition="top" textAlign="center">
+				<img src="/images/avatars/book-on-the-grass.jpg" alt="" />
+			</CardMedia>
+			<CardBody textAlign="center">
+				<CardTitle mb="1">Tallie Bettis</CardTitle>
+				<El textMuted>Director of Sales</El>
+			</CardBody>
+		</Card>
+	</El>
+
+	<El colMd="6" mb="3">
+		<Card>
+			<CardBody row alignItems="center">
+				<El col="auto">
+					<Avatar size="lg">
+						<img src="/images/avatars/3.jpg" alt="" />
+					</Avatar>
+				</El>
+				<El col>
+					<El mb="1" tag="h3">Paweł Kuna</El>
+					<El mb="1" textMuted>Working in Tabler</El>
+					<El textMuted><Dot color="success" /> Online</El>
+				</El>
+				<El col="auto">
+					<Button>Subscribe</Button>
+				</El>
+				<El col="auto">
+					<Dropdown placement="bottom-end" arrow={false}>
+						<Icon name="dots-vertical" slot="target" />
+						<DropdownMenu>
+							<DropdownItem href="#">Import</DropdownItem>
+							<DropdownItem href="#">Export</DropdownItem>
+							<DropdownItem href="#">Download</DropdownItem>
+							<DropdownItem href="#" textColor="danger">Delete</DropdownItem>
+						</DropdownMenu>
+					</Dropdown>
+				</El>
+			</CardBody>
+		</Card>
+	</El>
+	<El colMd="6" mb="3">
+		<Card>
+			<CardBody row alignItems="center">
+				<El col="auto">
+					<Avatar size="lg">
+						<img src="/images/avatars/6.jpg" alt="" />
+					</Avatar>
+				</El>
+				<El col>
+					<El mb="1" tag="h3">Mallory Hulme</El>
+					<El mb="1" textMuted>Working in Kare</El>
+					<El textMuted><Dot color="success" /> Online</El>
+				</El>
+				<El col="auto">
+					<Button>Subscribe</Button>
+				</El>
+				<El col="auto">
+					<Dropdown placement="bottom-end" arrow={false}>
+						<Icon name="dots-vertical" slot="target" />
+						<DropdownMenu>
+							<DropdownItem href="#">Import</DropdownItem>
+							<DropdownItem href="#">Export</DropdownItem>
+							<DropdownItem href="#">Download</DropdownItem>
+							<DropdownItem href="#" textColor="danger">Delete</DropdownItem>
+						</DropdownMenu>
+					</Dropdown>
+				</El>
+			</CardBody>
+		</Card>
+	</El>
+
+	<El colSm="6" colLg="3" mb="3">
+		<Card>
+			<CardBody row alignItems="center">
+				<El col="auto">
+					<Avatar bgColor="primary" textColor="light">
+						<Icon name="currency-dollar" />
+					</Avatar>
+				</El>
+				<El col>
+					<El mb="1" textMuted>132 Sales</El>
+					<El textMuted>12 waiting payments</El>
+				</El>
+			</CardBody>
+		</Card>
+	</El>
+	<El colSm="6" colLg="3" mb="3">
+		<Card>
+			<CardBody row alignItems="center">
+				<El col="auto">
+					<Avatar bgColor="success" textColor="light">
+						<Icon name="shopping-cart" />
+					</Avatar>
+				</El>
+				<El col>
+					<El mb="1" textMuted>78 Orders</El>
+					<El textMuted>32 shipped</El>
+				</El>
+			</CardBody>
+		</Card>
+	</El>
+	<El colSm="6" colLg="3" mb="3">
+		<Card>
+			<CardBody row alignItems="center">
+				<El col="auto">
+					<Avatar bgColor="danger" textColor="light">
+						<Icon name="user" />
+					</Avatar>
+				</El>
+				<El col>
+					<El mb="1" textMuted>1352 Members</El>
+					<El textMuted>163 registered today</El>
+				</El>
+			</CardBody>
+		</Card>
+	</El>
+	<El colSm="6" colLg="3" mb="3">
+		<Card>
+			<CardBody row alignItems="center">
+				<El col="auto">
+					<Avatar bgColor="yellow" textColor="light">
+						<Icon name="message" />
+					</Avatar>
+				</El>
+				<El col>
+					<El mb="1" textMuted>132 Comments</El>
+					<El textMuted>16 waitings</El>
+				</El>
+			</CardBody>
+		</Card>
+	</El>
 </El>
