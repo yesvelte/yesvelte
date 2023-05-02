9882e4489ce17763d5a6887045a870477113c6ad
TheHadiAhmadi <thehadiahmadi@gmail.com>
2023-05-03 00:48:42 +0430

    #382 add OnInput example for FormAutocomplete

---
+++ src/routes/docs/form/form-autocomplete/FormAutocompleteOnInput.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { El, FormAutocomplete, Avatar } from 'yesvelte'
+
+	let items: any[] = []
+
+	function debounced(callback: any, timeout = 300) {
+		let timer: any
+		return (...args: any[]) => {
+			clearTimeout(timer)
+			timer = setTimeout(() => callback(...args), timeout)
+		}
+	}
+
+	function onInput(query: string) {
+		var url = 'https://api.github.com/search/repositories?q=' + encodeURIComponent(query)
+
+		fetch(url)
+			.then((response) => response.json())
+			.then((json) => {
+				items = json?.items ?? []
+			})
+			.catch(() => {
+				items = []
+			})
+	}
+
+	const debouncedOnInput = debounced(onInput)
+
+	let value: any
+</script>
+
+
+<FormAutocomplete label="{items.length} items" {items} on:input={(event) => debouncedOnInput(event.detail)} bind:value let:item>
+	<El py="2" px="2" d="flex">
+		<Avatar me="3">
+			<img src={item.owner.avatar_url} />
+		</Avatar>
+        <El ms="2">
+			<El mb="1">
+				<El tag="h4" d="inline-block">
+					{item.name}
+				</El>
+				<El tag="span" textMuted>by {item.owner.login}</El>
+			</El>
+			<El>{item.description}</El>
+		</El>
+	</El>
+</FormAutocomplete>
+{#if value}
+	url: <El tag="a" href={value.html_url}>{value.html_url}</El>
+{/if}
