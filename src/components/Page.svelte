3c0bf30bd711eb8c7abee6ece96f7958bf0f3a13
Hadi Ahmadi <thehadiahmadi@gmail.com>
2023-05-20 20:02:51 +0430

    #258 add next and previous sections to docs

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-	import { Card, CardBody, El } from 'yesvelte'
+	import { Card, CardBody, El, Icon } from 'yesvelte'
 	import ToC from './ToC.svelte'
 	import { page } from '$app/stores'
 	export let title: string = ''
 	export let description: string = ''
+
+	$: nextItem = $page.data.links.nextItem
+	$: prevItem = $page.data.links.prevItem
 </script>
 
 <svelte:head>
@@ -22,8 +25,36 @@
 				<slot />
 			</CardBody>
 		</Card>
+		<El row mt="3" mb="5">
+			{#if prevItem}
+				<El col="auto" tag="a" href={prevItem.href}>
+					<El class="docs-link" tag="h3" mb="0">
+						<Icon name="chevron-left" />
+						{prevItem.title}
+					</El>
+				</El>
+			{/if}
+			{#if nextItem}
+				<El col="auto" ms="auto" justifyContent="end" tag="a" href={nextItem.href}>
+					<El class="docs-link" tag="h3" mb="0">
+						{nextItem.title}
+						<Icon name="chevron-right" />
+					</El>
+				</El>
+			{/if}
+		</El>
 	</El>
 	<El colLg="2" d="none" dLg="inline-block">
 		<ToC />
 	</El>
 </El>
+
+<style>
+	:global(.docs-link) {
+		cursor: pointer;
+	}
+
+	:global(.docs-link):hover {
+		text-decoration: none;
+	}
+</style>
