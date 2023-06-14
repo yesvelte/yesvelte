602eedf5f21548f4ee703a109e1944104a5f4e22
TheHadiAhmadi <thehadiahmadi@gmail.com>
2023-06-15 00:53:59 +0430

    #498 update docs layout

--- src/components/SidebarNavigations.svelte
+++ src/components/SidebarNavigations.svelte
@@ -16,7 +16,7 @@
 	export let pathname: string = ''
 </script>
 
-<Sidebar {...$$restProps}>
+<Sidebar class="y-docs-sidebar" {...$$restProps}>
 	{#each navigations as navigation}
 		{#if navigation.children}
 			<SidebarItem
