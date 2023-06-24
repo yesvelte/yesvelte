747b8f1698222eab3496ff32cfcfab8ed9d52ba5
TheHadiAhmadi <thehadiahmadi@gmail.com>
2023-06-24 21:52:35 +0430

    #538 implement focus trap for Modal and Offcanvas

---
+++ src/routes/docs/modal/ModalAutoClose.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { Button, Modal, ModalBody, ModalFooter } from 'yesvelte'
+	let show = false
+	let show2 = false
+	const toggleWithoutBackdrop = () => (show = !show)
+	const toggleWithBackdrop = () => (show2 = !show2)
+</script>
+
+<Button color="primary" on:click={toggleWithoutBackdrop}>AutoClose</Button>
+<Button color="primary" on:click={toggleWithBackdrop}>AutoClose without backdrop</Button>
+
+<Modal title="With backdrop" autoClose backdrop={false} bind:show={show2}>
+	<ModalBody>
+		This is an autoClose modal without backdrop. Click outside of the modal to close it.
+	</ModalBody>
+	<ModalFooter>
+		<Button color="primary" on:click={() => (show = false)}>OK</Button>
+	</ModalFooter>
+</Modal>
+
+<Modal title="Without backdrop" autoClose backdrop bind:show>
+	<ModalBody>
+		This is an autoClose modal with backdrop. Click outside of the modal to close it.
+	</ModalBody>
+	<ModalFooter>
+		<Button color="primary" on:click={() => (show = false)}>OK</Button>
+	</ModalFooter>
+</Modal>
--- src/routes/docs/modal/ModalBackdrop.svelte
+++ src/routes/docs/modal/ModalBackdrop.svelte
@@ -6,7 +6,7 @@
 
 <Button on:click={() => (show = true)} color="primary">Without Backdrop</Button>
 
-<Modal title="Without backdrop" backdrop={false} bind:show>
+<Modal title="Without backdrop" autoClose backdrop={false} bind:show>
 	<ModalBody>This is a modal without backdrop. Click outside of the modal to close it.</ModalBody>
 	<ModalFooter>
 		<Button color="primary" on:click={() => (show = false)}>OK</Button>
