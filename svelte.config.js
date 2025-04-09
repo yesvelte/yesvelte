b20deab1788eba9852f448250c519f8d8a8a5d9e
TheHadiAhmadi <thehadiahmadi@gmail.com>
2025-04-09 17:35:45 +0430

    Update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,27 @@
-import adapter from '@sveltejs/adapter-static'
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import path from 'path'
 import previewProcessor from './scripts/preview.js'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [previewProcessor(), vitePreprocess()],
+	preprocess: [
+		previewProcessor(),
+		vitePreprocess(),
+	],
 	kit: {
-		adapter: adapter({}),
-		prerender: {
-			handleMissingId: 'warn',
-			handleHttpError: 'warn',
-		},
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter(),
 		alias: {
-			$components: './src/components',
-			yesvelte: './src/lib',
-		},
-	},
-}
+			'$components': path.resolve('./src/doc-components'),
+			'yesvelte': path.resolve('./src/lib'),
+			
+		}
+	}
+};
 
-export default config
+export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,8 @@
-import path from 'path'
-// import { watchAndRun } from 'vite-plugin-watch-and-run'
-import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	server: { port: 3000 },
-	plugins: [
-		sveltekit(),
-		// watchAndRun([
-		// 	{
-		// 	  name: 'generate styles',
-		// 	  watchKind: ['add', 'change', 'unlink'],
-		// 	  watch: path.resolve('src/scss/**/*.scss'),
-		// 	  run: 'npm run style',
-		// 	  delay: 300,
-		// 	}
-		// ])
-	],
-})
+	plugins: [tailwindcss(), sveltekit()]
+});
