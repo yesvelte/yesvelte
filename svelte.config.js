27f108b0f95d25ada07c05aed9468057497b36c0
TheHadiAhmadi <thehadiahmadi@gmail.com>
2025-04-11 12:29:28 +0430

    use static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import path from 'path'
 import previewProcessor from './scripts/preview.js'
 
@@ -7,21 +7,17 @@ import previewProcessor from './scripts/preview.js'
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [
-		previewProcessor(),
-		vitePreprocess(),
-	],
+	preprocess: [previewProcessor(), vitePreprocess()],
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({}),
 		alias: {
-			'$components': path.resolve('./src/doc-components'),
-			'yesvelte': path.resolve('./src/lib'),
-			
-		}
-	}
-};
+			$components: path.resolve('./src/doc-components'),
+			yesvelte: path.resolve('./src/lib'),
+		},
+	},
+}
 
-export default config;
+export default config
